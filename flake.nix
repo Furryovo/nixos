6a85687bd08e29f422c8b82bfd524927b385a84a
Furryovo <111254531+Furryovo@users.noreply.github.com>
2026-02-20 06:51:40 +0000

    Refactor flake.nix to use structured inputs

--- flake.nix
+++ flake.nix
@@ -1,18 +1,43 @@
 {
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+    # https://github.com/NixOS/nixpkgs/tree/nixos-unstable
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
 
-  outputs = inputs: {
 
-    nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
+    # https://github.com/Gerg-L/spicetify-nix
+    spicetify-nix = {
+      type = "github";
+      owner = "Gerg-l";
+      repo = "spicetify-nix";
+      ref = "master";
+    };
+
+
+    # https://github.com/kamack38/sddm-greenleaf
+    sddm-greenleaf = {
+      type = "github";
+      owner = "kamack38";
+      repo = "sddm-greenleaf";
+      ref = "main";
+      flake = false;
+    };
+   };
+
+
+  outputs = { self, nixpkgs, sddm-greenleaf, spicetify-nix }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
       modules = [
-        { nix.settings.experimental-features = ["nix-command" "flakes"]; }
+        { nix.settings.experimental-features = [ "nix-command" "flakes" ]; }
         ./configuration.nix
       ];
     };
-
   };
-  
-}
+ }
+
+
