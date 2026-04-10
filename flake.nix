3ba603346324ff5a9a8fb527c2f6e804df70ca60
Furryovo <111254531+Furryovo@users.noreply.github.com>
2026-04-10 17:46:39 +0200

    Add NUR input and configure NUR modules
    Added NUR as an input and configured it in outputs.

--- flake.nix
+++ flake.nix
@@ -8,6 +8,49 @@
       ref = "nixos-unstable";
     };
 
+    # https://github.com/kamack38/sddm-greenleaf
+  #  sddm-greenleaf = {
+  #    type = "github";
+  #    owner = "kamack38";
+  #    repo = "sddm-greenleaf";
+  #    ref = "main";
+  #    flake = false;
+  #  };
+
+      # https://github.com/nix-community/NUR
+      NUR = {
+      type = "github";
+      owner = "nix-community";
+      repo = "NUR";
+      ref = "main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+
+  outputs = { self, nixpkgs, NUR, }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        { nix.settings.experimental-features = [ "nix-command" "flakes" ]; }
+        NUR.modules.nixos.default       
+          ./configuration.nix
+      ];
+    };
+  };
+ }
+
+
+{
+  inputs = {
+    # https://github.com/NixOS/nixpkgs/tree/nixos-unstable
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
+
 
     # https://github.com/Gerg-L/spicetify-nix
     spicetify-nix = {
