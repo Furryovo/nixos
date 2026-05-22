d04f8ae0f8842ed6f996d698e4134d09553f3c67
Furryovo <111254531+Furryovo@users.noreply.github.com>
2026-05-22 12:36:14 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,6 @@
   boot.kernelParams = [ "amdgpu.ppfeaturemask=0xffffffff" ];
 
   networking.hostName = "nixos"; # Define your hostname.
-  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
 
   # Enable networking
